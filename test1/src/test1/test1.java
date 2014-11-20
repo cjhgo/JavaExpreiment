8434763a840afcc79509c1b162e1fa84569f65ad
cjhgo <1728533217en@gmail.com>
2014-11-21 01:54:19 +0800

    test to github

---
+++ test1/src/test1/test1.java
@@ -0,0 +1,11 @@
+package test1;
+public   class test1
+{
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("my name is 陈军航\n");
+		System.out.println("my number is 1241901305\n");
+	}
+
+}
